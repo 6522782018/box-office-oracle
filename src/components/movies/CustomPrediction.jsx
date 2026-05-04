586833cfcf6b322982e6ef83506287d0f6edb9fb
Poramate Suphanklang <poramatesuphanklang@Xmass-MacBook-Air.local>
2026-05-04 18:23:57 +0700

    fix custom prediction api url

--- src/components/movies/CustomPrediction.jsx
+++ src/components/movies/CustomPrediction.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Plus, Trash2, Loader2 } from "lucide-react";
 
-const API_URL = "https://predict-movie-profit-backend.onrender.com/api/predict";
+const API_URL = "https://predict-movie-profit-backend.onrender.com";
 // Later for deployed version, change to:
 // const API_URL = "https://predict-movie-profit-backend.onrender.com";
 
